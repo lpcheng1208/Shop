57c8a3d7573a66e68979e0d31f2d2836889bd1a2
lpc1208 <lpc00972@gmail.com>
2018-07-17 18:49:41 +0800

    添加留言功能,用户中心功能的完善

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -9,7 +9,7 @@ from rest_framework import filters
 
 
 class GoodsPagination(PageNumberPagination):
-    page_size = 20
+    page_size = 50
     page_size_query_param = 'page_size'
     page_query_param = 'page'
     max_page_size = 100
